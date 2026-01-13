fc045851184f8135009c5c3f72dc6048fd52fee8
mammumammi <ashinmammu@gmail.com>
2026-01-13 13:22:18 +0530

    Added postgres via supabase to the project

--- backend/goatners-backend/src/app.module.ts
+++ backend/goatners-backend/src/app.module.ts
@@ -1,9 +1,32 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoginModule } from './login/login.module';
+import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule} from '@nestjs/config'
 
 @Module({
-  imports: [],
+  imports: [LoginModule, AuthModule,
+    ConfigModule.forRoot({
+      isGlobal:true,
+      envFilePath:'.env'
+    }),
+    TypeOrmModule.forRootAsync({
+      imports:[ConfigModule],
+      useFactory: async () => ({
+        type:'postgres',
+        url:"postgresql://postgres.iuetzhswcllrpxorftsl:Goatners@136@aws-1-ap-northeast-2.pooler.supabase.com:5432/postgres",
+        autoLoadEntities:true,
+        synchronize:false,
+        ssl:{
+          rejectUnauthorized: false
+        },
+        logging:true
+      }),
+
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
